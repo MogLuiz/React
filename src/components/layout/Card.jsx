d0455c7c9faa3583cc3406edcc4f747c715b9504
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-04-25 13:09:39 -0300

    Add files via upload

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,7 +2,13 @@ import React from "react"
 import "./Card.css"
 
 export default (props) => {
-    return <div className="Card">
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00',
+    }
+
+    return <div className="Card" style={cardStyle}>
         <div className="Title">
             {props.title}
         </div>
