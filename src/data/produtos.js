8d64320934bb7d7b16ebb242952ad293e2fc8efa
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-05-04 08:25:16 -0300

    Add files via upload

---
+++ src/data/produtos.js
@@ -0,0 +1,14 @@
+const produtos = [
+    { id: 1, nome: 'Macbook', Preco: 5800.99 },
+    { id: 2, nome: 'Iphone', Preco: 8900.99 },
+    { id: 3, nome: 'Ipad', Preco: 3999.99 },
+    { id: 4, nome: 'Samsung', Preco: 4000.99 },
+    { id: 5, nome: 'Marcador de Texto', Preco: 6.99 },
+    { id: 6, nome: 'Curso', Preco: 599.00 },
+    { id: 7, nome: 'Teclado', Preco: 129.99 },
+    { id: 8, nome: 'Mouse sem fio', Preco: 69.90 },
+    { id: 9, nome: 'Monitor', Preco: 500.00 },
+    { id: 10, nome: 'Cadeira Gamer', Preco: 899.99 },
+]
+
+export default produtos
